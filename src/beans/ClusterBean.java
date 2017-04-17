fbb26a5dc52253656790a803af7cc0bc2d317bd8
sayantanpolley <sayantan.polley@gmail.com>
2017-04-17 18:46:42 +0200

    changes

--- src/beans/ClusterBean.java
+++ src/beans/ClusterBean.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ClusterBean {
 	
-	public String clusterNum;
+	public String  clusterNum;
 	public String clusterURLs;
 	public String numURLs;
 	public List<String> arrayListofURLsPerCluster;
